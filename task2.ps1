022c23314213705c93124a5edc2ee8cd815262cb
Atski Apelsinner <morozov3695@mail.ru>
2022-03-01 22:28:45 +0300

    vtoraya taska gotova

---
+++ task2.ps1
@@ -0,0 +1,7 @@
+﻿param (
+[parameter(mandatory = $true)]
+[ValidateNotNullOrEmpty()] 
+[string]
+$str
+)
+$str.split() | Group-Object | Sort-Object -property "Count" -Descending | ForEach-Object {Write-Host "$($_.Name)" - "$($_.Count)"}
