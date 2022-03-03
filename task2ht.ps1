f8999745f95de9f2de0495fc69d76f782166c7a7
Atski Apelsinner <morozov3695@mail.ru>
2022-03-03 21:36:11 +0300

    таск 2 через хэш-таблицу

---
+++ task2ht.ps1
@@ -0,0 +1,9 @@
+﻿param (
+[parameter(mandatory = $true)]
+[ValidateNotNullOrEmpty()] 
+[string]
+$str
+)
+$myHT = [ordered]@{}
+$str.split() | Group-Object | ForEach-Object {$myHT.$($_.Name) = $($_.Count)}
+$myHT
